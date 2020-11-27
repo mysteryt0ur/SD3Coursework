c692c50c67d58f5cf2642dc70737ae5f109b246a
Eleanor Jones <Eleanor.Jones@rs-components.com>
2020-11-27 10:35:04 +0000

    setting fonts and sorting iso countdown

--- track-and-trace/src/components/IsoCountdown.js
+++ track-and-trace/src/components/IsoCountdown.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import ArrowIcon from '../images/arrow.png'
 
 class IsoCountdown extends React.Component {
 
     render() {
         return (
             <div>
-                <div>
-                    <h1>"hello"</h1>
+                <div className="iso-container">
+                    <div className="iso-header">
+                        <span>Isolation Countdown</span>
+                    </div>
+                    <div className="iso-info">
+                        <div className="iso-data">
+                            <span>Your isolation period ends in:</span>
+                        </div>
+                        <div className="iso-addit-info">
+                            <div>
+                                <p>Find out more information on isolation</p>
+                            </div>
+                            <div>
+                                <img src={ArrowIcon} alt="arrow-icon" id="arrow-icon"></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )

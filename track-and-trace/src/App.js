16f3b7155f0760d3bb40d21a04621253142796d8
Eleanor Jones <Eleanor.Jones@rs-components.com>
2020-12-04 10:04:34 +0000

    fixing home page button on venue check-in success page

--- track-and-trace/src/App.js
+++ track-and-trace/src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import React from 'react';
 import Dashboard from './pages/dashboard'
 import WelcomePage from './pages/welcome'
+import ProximityNotification from './components/ProximityNotification'
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      isUserRegistered: "true"
+      isUserRegistered: "true",
+      matchFound: ""
     };
+  }
+
+  getProximityInfo = () => {
+    setInterval(() => {
+      // get proximity data //
+    }, 1000)
+
+    // if (proximity data shows a match) {
+      // this.setState({ matchFound = "true" })
+    // }
+  }
+
+  componentDidMount() {
+    this.getProximityInfo()
   }
 
   render() {  
@@ -19,6 +35,11 @@ class App extends React.Component {
         {this.state.isUserRegistered === "false" &&
           <div>
             <WelcomePage />
+          </div>
+        }
+        {this.state.matchFound === "true" &&
+          <div>
+            <ProximityNotification />
           </div>
         } 
         {this.state.isUserRegistered === "true" &&
--- track-and-trace/src/pages/venue-check-in.js
+++ track-and-trace/src/pages/venue-check-in.js
@@ -7,86 +7,142 @@ class VenueCheckIn extends React.Component {
     constructor() {
         super();
         this.state = {
-            showCodeInput: "false",
+            useCamera: "true",
             hasCheckInFinished: "false",
-            venueName: "The Saxon Crown"
+            currentVenueCode: "",
+            venueName: "The Saxon Crown",
+            venueCodeLength: 10
         };
     }
 
-    changeShowCodeInput = (answer) => {
-        this.setState({ showCodeInput: answer })
+    getVenueName = (code) => {
+        // let result 
+        // check code against back-end to see if it matches up with T&T location //
+        //     if (the code matches) {
+        //         result = the venue name
+        //     } else {
+        //         result = false
+        //     }
+    }
+
+    updateCurrentCode = (event) => {
+        let currentCode = event.target.value
+        this.setState({ currentVenueCode : currentCode }); 
     }
 
     changeCheckInStatus = (answer) => {
         this.setState({ hasCheckInFinished: answer })
     }
 
-    getVenueName = (venue) => {
+    changeUseCamera = (answer) => {
+        this.setState({ useCamera: answer })
+    }
+
+    setVenueName = (venue) => {
         this.setState({ venueName: venue })
     }
 
+    getInputtedCodeLength = () => {
+        let iclReturn
+        if (this.state.currentVenueCode.length === this.state.venueCodeLength) {
+            iclReturn = true
+        } else {
+            iclReturn = false
+        } return iclReturn
+    }
+
     render() {
-        return (
-            <div className="addit-page">
-                <div className="page-header" id="vci-header">
-                    <p>Venue Check-In</p>
-                </div>
-                {this.state.hasCheckInFinished === "false" &&
-                <div className="content-inner-addit-page">
-                    <div className="page-info-venue">
-                        <p>Use your devices camera to place the QR code into the box below</p>
+        if (this.state.hasCheckInFinished === "false") {
+            return (
+                <div className="addit-page">
+                    <div className="page-header" id="vci-header">
+                        <p>Venue Check-In</p>
                     </div>
-                    <div id="camera-input">
-                        <div id="camera"></div>
-                    </div>
-                    <div className="page-info-venue-two" id="etcm-holder">
-                        <p id="vci-option-2" onClick={() => this.changeShowCodeInput("true")}>Enter the code manually</p>
-                    </div>
-                    <form id="venue-form-input">
-                        {this.state.showCodeInput === "true" && 
-                        <label id="input-test-code">
-                            <p>Venue Code: </p>
-                            <input type="text" name="name" className="text-input"/>
-                        </label>
-                        }
-                        <div className="submit-button-positioner">
-                            <div className="submit-button-holder" onClick={() => this.changeCheckInStatus("true")}>
-                                <div className="arrow-icon-holder">
-                                    <img src={ArrowIcon} alt="arrow-icon" className="arrow-icon-for-button"></img>
-                                </div>
-                                <input className="submit-button" type="submit" value="Continue" />
+                    <div className="content-inner-addit-page">
+                        <div className="page-info-venue">
+                            {this.state.useCamera === "true" &&
+                            <div>
+                                <p>Use your devices camera to place the QR code into the box below</p>
                             </div>
+                            }
+                            {this.state.useCamera === "false" &&
+                            <div>
+                                <p>Use the input box below to enter the code of the venue you want to check-into</p>
+                            </div>
+                            }
                         </div>
-                    </form>
-                </div>             
-                }
-                {this.state.hasCheckInFinished === "true" &&
-                <div id="success-page">
-                    <div className="page-header" id="cis-header">
+                        <div className="page-info-venue-two" id="vci-other-input-option">
+                            {this.state.useCamera === "true" &&
+                            <div>
+                                <p id="vci-option-2" onClick={() => this.changeUseCamera("false")}>Click here to enter the code manually</p>
+                            </div>
+                            }
+                            {this.state.useCamera === "false" &&
+                            <div>
+                                <p id="vci-option-2" onClick={() => this.changeUseCamera("true")}>Click here to enter the code via camera</p>
+                            </div>
+                            }
+                        </div>
+                        {this.state.useCamera === "true" && 
+                        <div id="camera-input">
+                            <div id="camera"></div>
+                        </div>
+                        }
+                        <form id="venue-form-input">
+                            {this.state.useCamera === "false" && 
+                            <div>
+                                <label id="input-test-code">
+                                    <p>Venue Code: </p>
+                                    <input type="text" value={this.state.currentVenueCode} name="name" className="text-input" onChange={this.updateCurrentCode}/>
+                                </label>
+                                {this.getInputtedCodeLength() === true &&
+                                <div>
+                                    <div id="returned-venue-name">
+                                        <p>You are trying to check-in to</p>
+                                        <p id="venue-name">{this.state.venueName}</p>
+                                    </div>
+                                    <p id="positive-message"><b>If this correct</b>, please click the continue button below.</p>
+                                    <p id="negative-message"><b>If this is not the right venue</b>, please check couble check the code you have entered.</p>
+                                </div>
+                                }
+                            </div>
+                            }
+                            <div className="submit-button-positioner">
+                                <div className="submit-button-holder" onClick={() => this.changeCheckInStatus("true")}>
+                                    <div className="arrow-icon-holder">
+                                        <img src={ArrowIcon} alt="arrow-icon" className="arrow-icon-for-button"></img>
+                                    </div>
+                                    <input className="submit-button" type="submit" value="Continue" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>  
+                </div>  
+            );
+        } else {
+            return (
+                <div className="success-page-holder">
+                    <div id="success-page">
+                        <div className="page-header" id="cis-header">
                         <p>Success</p>
                         <img src={SuccessTick} alt="success-tick" className="success-tick-icon"></img>
-                    </div>
-                    <div className="content-inner-addit-page" id="success-page-info">
-                        <div id="venue-info">
-                            <p>You have been checked into</p>
-                            <p id="venue-name">{this.state.venueName}</p>
-                            <p>Enjoy responsibly and in-line with current regulations</p>
-                            <p id="vci-learn-more">Learn more about current rules</p>
                         </div>
-                    </div>
-                    <div className="submit-button-positioner">
-                        <div className="submit-button-holder" id="home-button" onClick={() => this.changeCheckInStatus("true")}>
-                            <div className="arrow-icon-holder">
-                                <img src={BackArrowIcon} alt="back-arrow-icon" className="arrow-icon-for-button"></img>
+                        <div className="content-inner-addit-page" id="success-page-info">
+                            <div id="venue-info">
+                                <p>You have been checked into</p>
+                                <p id="venue-name">{this.state.venueName}</p>
+                                <p>Enjoy responsibly and in-line with current regulations</p>
+                                <p id="vci-learn-more">Learn more about current rules</p>
                             </div>
-                            <input className="submit-button" type="submit" value="Home" />
                         </div>
                     </div>
                 </div>
-                }
-            </div>
-        );
+            )
+        }
     }
 }
+
+
+
 
 export default VenueCheckIn;
